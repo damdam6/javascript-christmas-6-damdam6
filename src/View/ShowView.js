3344bd93e14f186a3e845d05f584696c2f6ef02d
damdam6 <dambi626626@gmail.com>
2023-11-14 17:24:04 +0900

    feat: set mainView

---
+++ src/View/ShowView.js
@@ -0,0 +1,16 @@
+import InputView from "./InputView";
+import OutputView from "./OutputView";
+
+
+const MainView = async function() {
+  let stage = 0;
+  switch(stage){
+    case 0 :
+      InputView.readDate();
+      break;
+    case 1 : 
+      //
+      break;
+
+  }
+}
