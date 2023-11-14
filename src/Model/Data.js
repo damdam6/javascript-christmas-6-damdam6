ce9c3477c893c4e29a0bdc314be319dc7713ca5f
damdam6 <dambi626626@gmail.com>
2023-11-14 09:00:50 +0900

    feat: FixedData 자료 추가

--- src/Model/Data.js
+++ src/Model/Data.js
@@ -8,29 +8,51 @@ const Data = {
   },
 };
 
-const FixedData = {
-  menuList: {
-    appetizers: {
+const FIXED_DATA = {
+  MENU_LIST: {
+    APPETIZERS: {
       양송이수프: 6000,
       타파스: 5500,
       시저샐러드: 8000
     },
-    mainCourses: {
+    MAIN_COURSES: {
       티본스테이크: 55000,
       바비큐립: 54000,
       해산물파스타: 35000,
       크리스마스파스타: 25000
     },
-    desserts: {
+    DESSERTS: {
       초코케이크: 15000,
       아이스크림: 5000
     },
-    beverages: {
+    BEVERAGES: {
       제로콜라: 3000,
       레드와인: 60000,
       샴페인: 25000
     }
+  },
+
+  SPECIAL_PROMOTION_DATE: {
+    STAR_DAY: [3, 10, 17, 24, 31, 25],
+    WEEKEND_DAY: [1, 2, 8, 9, 15, 16, 22, 23, 29, 30],
+    CHRISTMAS: 25
+  },
+
+  BADGE_PROMOTION_MIN_PRICE: {
+    STAR_BADGE : 5000,
+    TREE_BADGE : 10000,
+    SANTA_BADGE : 20000
+  },
+
+  PROMOTION_RULE : {
+    MIN_PRICE : 10000,
+    MIN_ORDER_CNT : 1,
+    MAX_ORDER_CNT : 20,
+    DATE_START : 1,
+    DATE_END : 31,
+    CHAMPAGNE_MIN_PRICE : 120000
   }
 };
 
-export { Data, FixedData };
+
+export { Data, FIXED_DATA };
