32de6659131c5ddfc750b8d5b94e8e18ae69e006
damdam6 <dambi626626@gmail.com>
2023-11-15 10:56:16 +0900

    feat: Data 초기화

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import MainView from './View/ShowView.js';
-
+import {Data, ProgramStage} from './Model/Data.js';
 class App {
   async run() {
+    Data.initailize();
+    ProgramStage.initailize();
     await MainView();
+    
   }
 }
 
