1c57de4fd38904dea95e72ad2d33b659728a17fe
damdam6 <dambi626626@gmail.com>
2023-11-15 11:42:46 +0900

    fix: validateMenu 수정

--- src/ModelView/GetInput.js
+++ src/ModelView/GetInput.js
@@ -5,14 +5,12 @@ const validateOrder = {
   duplicate(menu, menuObject){
     if (menuObject.hasOwnProperty(menu)) {
       throw ERROR_DATA.WRONG_MENU;
-      return false;
     }
     return true;
   },
   numeric(quantity) {
     if (isNaN(quantity)||quantity < FIXED_DATA.PROMOTION_RULE.MIN_ORDER_CNT) {
       throw ERROR_DATA.WRONG_MENU;
-      return;
     }
     return true;
   },
@@ -36,11 +34,24 @@ const validateOrder = {
       total += menuObject[menu];
     }
     if(total > FIXED_DATA.PROMOTION_RULE.MAX_ORDER_CNT){
-      throw ERROR_DATA.WRONG_DATE;
+      throw ERROR_DATA.WRONG_MENU;
     }
+  },
+  menuExits(menu){
+      const categories = Object.values(FIXED_DATA.MENU_LIST);
+      for (const category of categories) {
+        if(!(menu in category)) {
+          throw ERROR_DATA.WRONG_MENU;
+        }
+      }
+    },
+  validateOrder(menu, menuObject, quantity) {
+    this.duplicate(menu, menuObject);
+    this.numeric(quantity);
+    this.menuExits(menu);
   }
-
 }
+
 
 const validate = {
   dateValidate(input) {
@@ -59,20 +70,14 @@ const validate = {
   parseMenu(input) {  
     const items = input.split(',');
     const menuObject = {};
-  
     items.forEach(item => {
       const parts = item.split('-');
       if (parts.length !== 2) {
         throw ERROR_DATA.WRONG_MENU;
-        return;
       }
-  
       const [menu, quantityStr] = parts;
       const quantity = parseInt(quantityStr, 10);
-      validateOrder.duplicate(menu, menuObject);
-      validateOrder.numeric(quantity);
-
-
+      validateOrder.validateOrder(menu, menuObject, quantity);
       menuObject[menu] = quantity;
     });
     validateOrder.beverageOnly(menuObject);
